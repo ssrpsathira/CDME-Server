63f180daf3fa90225afd1273cd226a1fc0d5dad4
ssrp <ssrpsathira@gmail.com>
2014-12-14 11:51:58 +0530

    server improvements

--- ServiceProvider/NoiseDataService.php
+++ ServiceProvider/NoiseDataService.php
@@ -22,19 +22,42 @@ class NoiseDataService {
         
     }
 
-    protected function getDatabaseHandler() {
+    public function getDatabaseHandler() {
         if (!$this->databaseHandler) {
             $this->databaseHandler = new DatabaseHandler();
         }
         return $this->databaseHandler;
     }
 
-    protected function createDataUploadQuery($rawData) {
-        return "INSERT INTO `cdme_noise_data` (`location_id`, `noise_level`) VALUES (" . $rawData['location_id'] . ", " . $rawData['noise_level'] . ")";
+    public function isExistingLocation($longitude, $latitude) {
+        $existingLocationIdQuery = 'SELECT `id` FROM `cdme_location` WHERE `longitude`=' . $longitude . " AND `latitude`=" . $latitude . ' LIMIT 1;';
+        $existingLocationIdArray = $this->getDatabaseHandler()->executeQuery($existingLocationIdQuery);
+        if (!empty($existingLocationIdArray)) {
+            return $existingLocationIdArray[0]['id'];
+        } else {
+            return null;
+        }
     }
 
-    protected function createDataDownloadQuery($rawData) {
-        return "SELECT * FROM `cdme_noise_data` ORDER BY `date_time` ASC;";
+    public function saveCurrentLocation($longitude, $latitude) {
+        $locationInsertQuery = "INSERT INTO `cdme_location` (`longitude`, `latitude`) VALUES (" . $longitude . "," . $latitude . ");";
+        $this->getDatabaseHandler()->executeQuery($locationInsertQuery);
+    }
+
+    protected function createDataUploadQuery($rawData) {
+        $existingLocationId = $this->isExistingLocation($rawData['longitude'], $rawData['latitude']);
+        if ($existingLocationId) {
+            $dataInsertQuery = "INSERT INTO `cdme_noise_data` (`user_id`, `location_id`,`noise_level`,`date_time`) VALUES (NULL,'" . $existingLocationId . "', '" . $rawData['noise_level'] . "','" . $rawData['date_time'] . "');";
+        } else {
+            $this->saveCurrentLocation($rawData['longitude'], $rawData['latitude']);
+            $newLocationId = $this->isExistingLocation($rawData['longitude'], $rawData['latitude']);
+            $dataInsertQuery = "INSERT INTO `cdme_noise_data` (`user_id`, `location_id`,`noise_level`,`date_time`) VALUES (NULL, '" . $newLocationId . "', '" . $rawData['noise_level'] . "','" . $rawData['date_time'] . "');";
+        }
+        return $dataInsertQuery;
+    }
+
+    public function createDataDownloadQuery($rawData) {
+        return "SELECT * FROM `cdme_noise_data` LEFT JOIN `cdme_location` ON `cdme_noise_data`.`location_id` = `cdme_location`.`id` ORDER BY `cdme_noise_data`.`date_time` ASC;";
     }
 
     public function initializeDataUploadService($rawData) {
