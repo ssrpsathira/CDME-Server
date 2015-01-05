8e32df96b74322dce0c0caa0c7a5abd50a3a672b
ssrp <ssrpsathira@gmail.com>
2015-01-05 22:04:48 +0530

    fix kml style creating issue

--- task/generateAdmin2RegionKmlLayer.php
+++ task/generateAdmin2RegionKmlLayer.php
@@ -4,8 +4,9 @@ require_once dirname(__FILE__) . '/../Engine/DatabaseHandler.php';
 
 $dbHandler = new DatabaseHandler();
 
-$getStatisticalDataQuery = "SELECT DISTINCT(`cdme_admin_2_region_statistics`.region_id), `cdme_admin_2_region`.name, `cdme_admin_2_region_statistics`.mod, `cdme_admin_2_region_statistics`.median, `cdme_admin_2_region_statistics`.mean, `cdme_admin_2_region_statistics`.sd FROM `cdme_admin_2_region_statistics` LEFT JOIN `cdme_admin_2_region` ON `cdme_admin_2_region`.id = `cdme_admin_2_region_statistics`.region_id ORDER BY `cdme_admin_2_region_statistics`.date_time DESC";
+$getStatisticalDataQuery = "SELECT DISTINCT(`cdme_admin_2_region_statistics`.`region_id`), `cdme_admin_2_region`.`name`, `cdme_admin_2_region_statistics`.`mod`, `cdme_admin_2_region_statistics`.`median`, `cdme_admin_2_region_statistics`.`mean`, `cdme_admin_2_region_statistics`.`sd` FROM `cdme_admin_2_region_statistics` LEFT JOIN `cdme_admin_2_region` ON `cdme_admin_2_region`.`id` = `cdme_admin_2_region_statistics`.`region_id` GROUP BY `cdme_admin_2_region_statistics`.`region_id` ORDER BY `cdme_admin_2_region_statistics`.`date_time` DESC";
 $results = $dbHandler->executeQuery($getStatisticalDataQuery);
+var_dump($results);die;
 $kmlStyleText = '';
 $placeMarkText = '';
 foreach ($results as $result) {
